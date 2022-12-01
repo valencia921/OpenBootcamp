87a2d7d35070addca5d699c9a22a9b47056e99a8
valencia921 <dmvalenciah_1@uqvirtual.edu.co>
2022-12-01 14:34:21 -0500

    Primer commit

---
+++ src/src/Entrega_1/PrimerEntrega.java
@@ -0,0 +1,16 @@
+package Entrega_1;
+
+public class PrimerEntrega {
+    public static void main(String[] args) {
+        int entero = 12;
+        double doble= 23.3;
+        String texto = "Hola botcamp";
+        long largo = 232454L;
+
+        System.out.println("Soy un entero: "+ entero);
+        System.out.println("Soy un double: "+ doble);
+        System.out.println("Soy un texto:  "+ texto);
+        System.out.println("Soy un long:   "+ largo);
+
+    }
+}
---
+++ src/src/Precio_Iva/Precio_Iva.java
@@ -0,0 +1,15 @@
+package Precio_Iva;
+
+public class Precio_Iva {
+    public static void main(String[] args) {
+    int precio_iva = 0;
+    precio_iva = precio(100000,10);
+        System.out.println(precio_iva);
+    }
+    static int precio (int precio, int iva){
+        return precio *iva/100 ;
+    }
+
+}
+
+
